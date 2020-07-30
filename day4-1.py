053bcb144a5d0bf59cd8f4770c6f09e49b1d68fc
ryan990122 <68846072+ryan990122@users.noreply.github.com>
2020-07-30 14:24:48 +0800

    Add files via upload

---
+++ day4-1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 30 09:23:44 2020
+
+@author: user
+"""
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+number=1
+x,y,z=mc.player.getPos()
+for i in range(8):
+    for j in range(number):
+        mc.spawnEntity(x,y,z,60)
+    number=number*2
+    mc.postToChat("現在有"+str(number)+"隻蠹魚")
